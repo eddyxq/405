a2095c1b77efcfa3060ddef9564921d3075051eb
Time2Trade <eddy.qiang1@ucalgary.ca>
2019-09-17 11:53:20 -0700

    Initial Commit

---
+++ MyOptions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyOptions
+//
+//  Created by Eddy on 2019-09-17.
+//  Copyright © 2019 Eddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
