33503f9e8fc8392ba60d8eb936fe203c2a83d1aa
Ali Alkhazaly <ali.alkhazaly@ucalgary.ca>
2019-11-01 17:43:05 -0600

    Page Renaming, GUI changes
    Integrated portions of eddy branch

--- Profit_Intel/Controllers/HomeController.cs
+++ Profit_Intel/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Profit_Intel.Controllers
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult Analyze()
         {
             ViewData["Message"] = "Your application description page.";
 
@@ -29,7 +29,7 @@ namespace Profit_Intel.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Portfolio()
         {
             return View();
         }
